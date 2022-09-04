4b33b0426bd12ee1af292c9ad675a0c2bc185b0b
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-03 20:30:02 -0400

    Project structure changed to include both back-end and front-end files

--- ys_ecommerce/src/main/java/ys/ecommerce/YsEcommerceApplication.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/YsEcommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class YsEcommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
+        SpringApplication.run(YsEcommerceApplication.class, args);
     }
 
 }
