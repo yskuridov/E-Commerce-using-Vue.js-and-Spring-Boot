458cac2cd239a5f3738751e0af363a85c9edb9e2
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 19:39:55 -0400

    Customer class implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/model/User/Customer.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/User/Customer.java
@@ -1,5 +1,11 @@
 package ys.ecommerce.model.User;
 
 public class Customer extends User{
+    public Customer(String username, String password, String firstName, String lastName, String address) {
+        super(username, password, firstName, lastName, address);
+        //List<Product> cart;
+        //List<Order> orders;
 
+
+    }
 }
