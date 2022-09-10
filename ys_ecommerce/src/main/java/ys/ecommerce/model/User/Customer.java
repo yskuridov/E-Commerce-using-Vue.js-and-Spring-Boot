c3085b0d98521d637f827af2c0a3146debd9e59a
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 19:49:12 -0400

    Added persistence elements to Customer class

--- ys_ecommerce/src/main/java/ys/ecommerce/model/User/Customer.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/User/Customer.java
@@ -1,6 +1,18 @@
 package ys.ecommerce.model.User;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("CUSTOMER")
+@Data
+@NoArgsConstructor
+
 public class Customer extends User{
+
     public Customer(String username, String password, String firstName, String lastName, String address) {
         super(username, password, firstName, lastName, address);
         //List<Product> cart;
