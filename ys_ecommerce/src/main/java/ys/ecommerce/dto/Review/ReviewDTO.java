4f58f96a00ce853ff8329f6d040c19d44646c2b5
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 22:28:56 -0400

    ReviewDTO implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/dto/Review/ReviewDTO.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/dto/Review/ReviewDTO.java
@@ -1,4 +1,20 @@
 package ys.ecommerce.dto.Review;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Review.Review;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewDTO {
+    private String id;
+    private String customer;
+    private String vendor;
+    private String rating;
+
+    public ReviewDTO(Review review){
+        this(Long.toString(review.getId()), Long.toString(review.getCustomer().getId()), Long.toString(review.getVendor().getId()), Integer.toString(review.getRating()));
+    }
 }
