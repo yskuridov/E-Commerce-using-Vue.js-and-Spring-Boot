43da8836d8e13d8f85bc7b176b40c19520fb964d
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 20:58:05 -0400

    Comment class implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/model/Review/Comment.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/Review/Comment.java
@@ -1,4 +1,32 @@
 package ys.ecommerce.model.Review;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.User.Customer;
+import ys.ecommerce.model.User.Vendor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+
 public class Comment {
+    @Id
+    @GeneratedValue
+    private long id;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Vendor vendor;
+    private String comment;
+
+    public Comment(Customer writtenBy, Vendor writtenTo, String comment){
+        this.customer = writtenBy;
+        this.vendor = writtenTo;
+        this.comment = comment;
+    }
 }
