c471aa1f8003f5cc9797a0455bde5d239cb43806
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 21:25:05 -0400

    Vendor class modified to include reviews and inventory

--- ys_ecommerce/src/main/java/ys/ecommerce/model/User/Vendor.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/User/Vendor.java
@@ -3,22 +3,29 @@ package ys.ecommerce.model.User;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Product.Product;
+import ys.ecommerce.model.Review.Review;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("VENDOR")
 @Data
 @NoArgsConstructor
 public class Vendor extends User {
+    @OneToMany
+    private List<Review> reviews;
+    @OneToMany
+    private List<Product> inventory;
+    private boolean hasOpenComments;
     public Vendor(String username, String password, String firstName, String lastName, String address){
         super(username, password, firstName, lastName, address);
         double rating;
-        //List<Review> reviews;
-        boolean hasOpenComments = false;
-        //List<Product> products;
-
+        reviews = new ArrayList<>();
+        hasOpenComments = true;
     }
 }
