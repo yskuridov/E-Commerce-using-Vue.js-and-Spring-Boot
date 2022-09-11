fd2e42a05471be789dd27fdce052e079d24014f4
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 20:38:25 -0400

    Product class implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/model/Product/Product.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/Product/Product.java
@@ -1,4 +1,36 @@
 package ys.ecommerce.model.Product;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.User.Vendor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+
 public class Product {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private double deliveryFee;
+    private int stock;
+    @ManyToOne
+    private Vendor soldBy;
+
+    public Product(String name, String description, double price, double deliveryFee, int stock, Vendor vendor){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.deliveryFee = deliveryFee;
+        this.stock = stock;
+        this.soldBy = vendor;
+    }
 }
