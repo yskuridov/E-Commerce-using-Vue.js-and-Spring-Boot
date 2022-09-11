0627e5a4c302af600f70b0171dc89ac5426e9854
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 21:21:43 -0400

    Variables added to Customer class

--- ys_ecommerce/src/main/java/ys/ecommerce/model/User/Customer.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/User/Customer.java
@@ -2,9 +2,14 @@ package ys.ecommerce.model.User;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Order.Order;
+import ys.ecommerce.model.Product.Product;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("CUSTOMER")
@@ -12,12 +17,13 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 
 public class Customer extends User{
-
+    @OneToMany
+    List<Product> cart;
+    @OneToMany
+    List<Order> orders;
     public Customer(String username, String password, String firstName, String lastName, String address) {
         super(username, password, firstName, lastName, address);
-        //List<Product> cart;
-        //List<Order> orders;
-
-
+        cart = new ArrayList<>();
+        orders = new ArrayList<>();
     }
 }
