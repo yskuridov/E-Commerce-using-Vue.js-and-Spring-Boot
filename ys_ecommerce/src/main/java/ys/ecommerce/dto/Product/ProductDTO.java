7507fc28b56f2673ae0d0dc3976f051a5a413bcb
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 22:15:07 -0400

    ProductDTO implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/dto/Product/ProductDTO.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/dto/Product/ProductDTO.java
@@ -1,4 +1,23 @@
 package ys.ecommerce.dto.Product;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Product.Product;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+    private String id;
+    private String name;
+    private String description;
+    private String price;
+    private String deliveryFee;
+    private String stock;
+    private String soldBy;
+
+    public ProductDTO(Product product){
+        this(Long.toString(product.getId()), product.getName(), product.getDescription(), Double.toString(product.getPrice()), Double.toString(product.getDeliveryFee()), Integer.toString(product.getStock()), Long.toString(product.getSoldBy().getId()));
+    }
 }
