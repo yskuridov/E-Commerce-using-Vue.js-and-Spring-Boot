258a9cfa3a0316e07269b308050f04b5c0b660fb
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 23:55:29 -0400

    VendorRepository implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/repository/VendorRepository.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/repository/VendorRepository.java
@@ -1,4 +1,11 @@
 package ys.ecommerce.repository;
 
-public class VendorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ys.ecommerce.model.User.Vendor;
+
+import java.util.Optional;
+
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    Optional<Vendor> getVendorByUsername(String username);
+
 }
