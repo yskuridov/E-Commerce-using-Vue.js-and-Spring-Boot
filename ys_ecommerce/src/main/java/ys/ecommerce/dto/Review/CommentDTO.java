5d8df86149997478d7e9f125722c3e4d7c2e94b1
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 22:17:41 -0400

    CommentDTO implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/dto/Review/CommentDTO.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/dto/Review/CommentDTO.java
@@ -1,4 +1,20 @@
 package ys.ecommerce.dto.Review;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Review.Comment;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDTO {
+    private String id;
+    private String customer;
+    private String vendor;
+    private String comment;
+
+    public CommentDTO(Comment comment){
+        this(Long.toString(comment.getId()), Long.toString(comment.getCustomer().getId()), Long.toString(comment.getVendor().getId()), comment.getComment());
+    }
 }
