252dfb209a314cedf86ed1003e26f1fcf373a332
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 22:10:34 -0400

    OrderDTO implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/dto/Order/OrderDTO.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/dto/Order/OrderDTO.java
@@ -1,4 +1,21 @@
 package ys.ecommerce.dto.Order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Order.Order;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
+    private String id;
+    private String buyer;
+    private String seller;
+    private String orderDate;
+    private String deliveryDate;
+
+    public OrderDTO(Order order){
+        this(Long.toString(order.getId()), Long.toString(order.getBuyer().getId()), Long.toString(order.getSeller().getId()), order.getOrderDate().toString(), order.getDeliveryDate().toString());
+    }
 }
