941856a593cac802d64ec761b70d190b984c053c
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 20:32:19 -0400

    Order class implemented

--- ys_ecommerce/src/main/java/ys/ecommerce/model/Order/Order.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/Order/Order.java
@@ -1,4 +1,29 @@
 package ys.ecommerce.model.Order;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.Product.Product;
+import ys.ecommerce.model.User.Customer;
+import ys.ecommerce.model.User.Vendor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue
+    private long id;
+    @ManyToOne
+    private Customer buyer;
+    @ManyToOne
+    private Vendor seller;
+    //@OneToMany
+    //List<Product> products;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+
 }
