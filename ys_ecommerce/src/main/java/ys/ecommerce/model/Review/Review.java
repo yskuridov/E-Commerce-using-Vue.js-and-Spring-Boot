e88c895bbab4aa3685df7c5a91611decf90cfb2a
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-10 21:07:22 -0400

    Review class implemented + added validation dependency in pom.xml

--- ys_ecommerce/src/main/java/ys/ecommerce/model/Review/Review.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/model/Review/Review.java
@@ -1,4 +1,27 @@
 package ys.ecommerce.model.Review;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ys.ecommerce.model.User.Customer;
+import ys.ecommerce.model.User.Vendor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Max;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Review {
+    @Id
+    @GeneratedValue
+    private long id;
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Vendor vendor;
+    @Max(value=10)
+    private int rating;
 }
