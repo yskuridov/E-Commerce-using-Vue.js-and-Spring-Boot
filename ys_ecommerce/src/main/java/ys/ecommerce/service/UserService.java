be9aaab3dc700e228f2f098f22886dfcb2c4bd5b
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-09-18 16:32:12 -0400

    User service implemented for the creation of new users

--- ys_ecommerce/src/main/java/ys/ecommerce/service/UserService.java
+++ ys_ecommerce/src/main/java/ys/ecommerce/service/UserService.java
@@ -1,4 +1,31 @@
 package ys.ecommerce.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ys.ecommerce.dto.User.UserDTO;
+import ys.ecommerce.model.User.Customer;
+import ys.ecommerce.model.User.Vendor;
+import ys.ecommerce.repository.CustomerRepository;
+import ys.ecommerce.repository.VendorRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
+
+    public Optional<UserDTO> createVendorAccount(UserDTO user){
+        Vendor vendor = new Vendor(user.getUsername(), user.getPassword(), user.getFirstName(), user.getLastName(), user.getAddress());
+        vendorRepository.save(vendor);
+        return Optional.of(new UserDTO(vendor));
+    }
+
+    public Optional<UserDTO> createCustomerAccount(UserDTO user){
+        Customer customer = new Customer(user.getUsername(), user.getPassword(), user.getFirstName(), user.getLastName(), user.getAddress());
+        customerRepository.save(customer);
+        return Optional.of(new UserDTO(customer));
+    }
+
 }
