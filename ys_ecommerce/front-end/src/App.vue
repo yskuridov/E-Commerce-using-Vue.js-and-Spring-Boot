34fe8ce4b72518adc236a0664342f2fa478c3e73
yskuridov <ys@Yegors-MacBook-Pro.local>
2022-10-06 10:01:40 -0400

    Front-end project initialized + navbar HTML

---
+++ ys_ecommerce/front-end/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="m-0">
+    <NavBar />
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/Navbar.vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ys_ecommerce/front-end/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark">
+            <b-navbar-brand href="#">
+              <b-icon icon="list-nested" aria-hidden="true"></b-icon>
+              Seller
+            </b-navbar-brand>
+            <b-nav>
+                <b-nav-item>Products</b-nav-item>
+                <b-nav-item>Profile</b-nav-item>
+            </b-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item-dropdown text="Search products by" right>
+                    <b-dropdown-item href="#">Product price</b-dropdown-item>
+                    <b-dropdown-item href="#">Vendor name</b-dropdown-item>
+                    <b-dropdown-item href="#">Delivery fee</b-dropdown-item>
+                    <b-dropdown-item href="#">Product description</b-dropdown-item>
+                    <b-dropdown-item href="#">Product name</b-dropdown-item>
+                </b-nav-item-dropdown>
+                <b-nav-form>
+                  <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                  <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
+<style lang="">
+    
+</style>
