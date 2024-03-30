177e529dbbb7c6195cc88178190f255b9a4067e6
Yegor <89847927+yskuridov@users.noreply.github.com>
2024-03-30 15:02:17 -0400

    Delete ys_ecommerce/Vue directory

--- ys_ecommerce/Vue/src/main.js
+++
@@ -1,8 +0,0 @@
-
-//src/main.js
-import{ createApp } from "vue";
-import App from "./App.vue";
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-
-createApp(App).mount("#app");
